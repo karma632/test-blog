fb7c951c7887267d47a9e3279629e05dc50cdc52
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-08-27 14:49:57 +0100

    change in postcard

--- components/postcard.tsx
+++ components/postcard.tsx
@@ -70,6 +70,7 @@ export default function PostCards() {
   // GET BOOKMARKS
   const { data: bookmarks } = useQuery({
     queryKey: ["bookmarks"],
+    enabled: !!session?.user,
     queryFn: async () => {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/bookmarks`,
