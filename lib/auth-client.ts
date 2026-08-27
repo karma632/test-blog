0fe2b71dd28873409e157cfa27c18f34a090a9c6
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-08-27 17:10:44 +0100

    new commit3

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    basePath: "/api/auth",
+    basePath: process.env.NEXT_PUBLIC_API_URL,
 
     
 });
