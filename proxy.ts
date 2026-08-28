4f21d458f52ffe0dc1f6c2828b985fb3cfa172b2
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-08-28 12:41:42 +0100

    back normal

--- proxy.ts
+++ proxy.ts
@@ -1,54 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-//   const secureCookie = request.cookies.get(
-//     "__Secure-better-auth.session_token"
-//   );
+  const protectedRoutes = ["/dashboard", "/bookmarks"];
 
-//   const normalCookie = request.cookies.get(
-//     "better-auth.session_token"
-//   );
+  if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+    const sessionCookie = request.cookies.get(
+      "__Secure-better-auth.session_token"
+    );
 
-//   console.log("===== PROXY CHECK =====");
-//   console.log("PATH:", pathname);
-//   console.log(
-//     "__Secure-better-auth.session_token:",
-//     secureCookie ? "FOUND" : "NOT FOUND"
-//   );
-//   console.log(
-//     "better-auth.session_token:",
-//     normalCookie ? "FOUND" : "NOT FOUND"
-//   );
+    if (!sessionCookie) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
 
-//   const protectedRoutes = ["/dashboard", "/bookmark"];
+  return NextResponse.next();
+}
 
-//   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-//     const sessionCookie = secureCookie || normalCookie;
-
-//     console.log(
-//       "PROTECTED ROUTE:",
-//       protectedRoutes.some((route) => pathname.startsWith(route))
-//     );
-
-//     console.log(
-//       "SESSION COOKIE:",
-//       sessionCookie ? "FOUND → ALLOW" : "NOT FOUND → REDIRECT"
-//     );
-
-//     if (!sessionCookie) {
-//       console.log("🚨 REDIRECTING TO /");
-//       return NextResponse.redirect(new URL("/", request.url));
-//     }
-//   }
-
-//   console.log("✅ PROXY ALLOWING REQUEST");
-//   console.log("======================");
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/dashboard/:path*", "/bookmark/:path*"],
+export const config = {
+  matcher: ["/dashboard/:path*", "/bookmarks/:path*"],
 };
