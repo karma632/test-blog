8ecdd4455f5af1c3c5e5ab61a009b49ab9fc8a03
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-09-01 09:29:15 +0100

    little ui fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -308,6 +308,9 @@ export default function NavBar() {
               </Link>
             ) : (
               <>
+                 <p className="px-3 py-2 text-sm font-medium text-red-500">
+                    {role}
+                  </p>
                 {/* POSTS */}
                 <Link
                   href="/posts"
